20b0bbfc87cba97826670241b8c7cf16422cbe50
EliDev <oscarmateoelias@gmail.com>
2024-10-09 10:11:01 -0600

    feat: Edit and View Product details

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import {useNavigate} from "react-router-dom";
 import {Product} from "../types";
 import {formatCurrency} from "../utils";
 
@@ -6,6 +7,8 @@ type ProductDetailsProps = {
 }
 
 export default function ProductDetails({product}: ProductDetailsProps) {
+    const navigate = useNavigate();
+
     const isAvailable = product.avilability;
 
     return (
@@ -20,6 +23,21 @@ export default function ProductDetails({product}: ProductDetailsProps) {
                 {isAvailable ? 'Available' : 'Not Available'}
             </td>
             <td className="p-3 text-lg text-gray-800 ">
+                <div className={"flex gap-2 items-center"}>
+                    {/*<Link*/}
+                    {/*    to={`/products/${product.id}/edit`}*/}
+                    {/*    className={"bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"}*/}
+                    {/*>Edit</Link>*/}
+                    <button
+                        onClick={() => navigate(`/products/${product.id}/edit`, {
+                            state: {
+                                product
+                            }
+                        })}
+                        className={"bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"}
+                    >Edit
+                    </button>
+                </div>
             </td>
         </tr>
     );
